c264bcee02037f058261c590f54c74d18b2b3ab8
Christophe Schmitz <cschmitz@redhat.com>
2024-06-05 16:39:00 +1000

    clenaup

--- env.sh
+++ env.sh
@@ -4,5 +4,7 @@ ARGO_CLUSTER_NAME=hub
 REGION=ap-southeast-2
 MASTER_INSTANCE_TYPE=m6i.4xlarge
 WORKER_INSTANCE_TYPE=m6i.2xlarge
+#MASTER_INSTANCE_TYPE=c5n.metal
+#WORKER_INSTANCE_TYPE=c5n.metal
 ARGO_CONFIG=~/.aws/argo-config
 HUB_KUBECONFIG=~/.aws/hub-kubeconfig
